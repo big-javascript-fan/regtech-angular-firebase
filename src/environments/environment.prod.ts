84a0c6dd24bac5b9838f39fb5c9a38f57bfd4def
javascripFan <wdeveloping9099@gmail.com>
2019-06-02 18:48:48 -0700

    configuration for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyALunk05AfCBiQB_3NG8--EHrxuIw9mz1g",
+    authDomain: "fcc-book-trading-2a293.firebaseapp.com",
+    databaseURL: "https://fcc-book-trading-2a293.firebaseio.com",
+    projectId: "fcc-book-trading-2a293",
+    storageBucket: "fcc-book-trading-2a293.appspot.com",
+    messagingSenderId: "465985532392",
+    appId: "1:465985532392:web:8a2dbe4f275db31e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyALunk05AfCBiQB_3NG8--EHrxuIw9mz1g",
+    authDomain: "fcc-book-trading-2a293.firebaseapp.com",
+    databaseURL: "https://fcc-book-trading-2a293.firebaseio.com",
+    projectId: "fcc-book-trading-2a293",
+    storageBucket: "fcc-book-trading-2a293.appspot.com",
+    messagingSenderId: "465985532392",
+    appId: "1:465985532392:web:8a2dbe4f275db31e"
+  }
 };
